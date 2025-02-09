1b720dea0103b7f4d2580461cad5ac733d5236a2
CrisMesina <cristophermatiasmesina@gmail.com>
2025-02-09 14:22:45 -0300

    cambio de textos

--- src/components/GoServices.tsx
+++ src/components/GoServices.tsx
@@ -7,13 +7,19 @@ export const GoServices = () => {
           <h1 className="text-center font-extrabold text-2xl">Conoce nuestros Servicios!</h1>
             <div className="flex font-bold text-2xl">
                 <div className="flex w-full phone-sm:w-100 phone-md:w-[500px] phone-lg:w-[600px] md:w-full text-center mx-2 mt-18">
-                    <p className="w-full mt-20 text-sm md:text-2xl">
-                        A dia de hoy contamos con un minimo de 5 servicios activos 
-                        que podemos otorgar. Con el tiempo puede que se incorporen mas servicios 
-                        con los que podamos ayudarte en un futuro proximo.
+                    <p className="w-full mt-20 text-sm md:text-xl">
+                        Nuestro sello es hacer labores de Fabricación y Montaje de soluciones
+                        en acero inoxidable y carbono, en las siguientes areas:
+                        <div className="mr-[500px] w-full text-lg mt-2">
+                            <p>- Piping en acero inoxidable y acero carbono.</p>
+                            <p>- Estructuras y Plataformas Acero Carbono e Inoxidable.</p>
+                            <p>- Transportadores de banda para producto granel y cajas.</p>
+                            <p>- Corte, Plegados y Corte CNC plasma y Oxicorte.</p>
+                        </div>
                     </p>
                     <img id="imagen" src="/nosotros.jpg" className="md:ml-40 invisible md:visible w-0 md:w-[450px] rounded my-10"/>
                 </div> 
+                
             </div>
             
             <div className="w-full phone-sm:w-100 phone-md:w-[500px] phone-lg:w-[600px] mb-20 mt-10 md:mb-0 md:-mt-10">
